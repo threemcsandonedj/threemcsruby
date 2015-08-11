88729e774ff2bcd6be6cb2b78ed5f32f84350f5a
Joseph Pollard <joseph.w.pollard@gmail.com>
2015-08-11 13:42:12 -0400

    Added custom titles to the pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | 3 MCs & 1 DJ"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | 3 MCs & 1 DJ"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | 3 MCs & 1 DJ"
   end
 
 end
